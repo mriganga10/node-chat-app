e4e38fe36257bc3e98cd6261a634804698b88f1e
Mriganga 10 <mriganga.deka1996@gmail.com>
2017-12-20 14:20:13 -0800

    broadcasted first

--- server/server.js
+++ server/server.js
@@ -14,18 +14,17 @@ app.use(express.static(publicPath));
 io.on('connection', (socket) => {
   console.log('New user connected');
 
-  // socket.emit('newMessage', {   //sends only to the socket where as io.emit sends to everybody connected
-  //   from: 'Admin',
-  //   text: 'Welcome to the chat app',
-  //   createdAt: new Date().getTime()
-  // });
+  socket.emit('newMessage', {   //sends only to the socket where as io.emit sends to everybody connected
+    from: 'Admin',
+    text: 'Welcome to the chat app',
+    createdAt: new Date().getTime()
+  });
 
-  // socket.broadcast.emit('newMessage', {
-  //   from: 'Admin',
-  //   text: 'New user joined',
-  //   createdAt: new Date().getTime()
-  // });
-
+  socket.broadcast.emit('newMessage', {
+    from: 'Admin',
+    text: 'New user joined',
+    createdAt: new Date().getTime()
+  });
   socket.on('createMessage', (message) => {
 	    console.log('createMessage', message);
 	    io.emit('newMessage', {
