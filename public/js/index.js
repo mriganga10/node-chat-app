341f2392bb77f8ce16291f292fd81fffeea496d3
Mriganga 10 <mriganga.deka1996@gmail.com>
2017-12-21 14:12:38 -0800

    scroll

--- public/js/index.js
+++ public/js/index.js
@@ -1,5 +1,21 @@
 var socket = io();
 
+function scrollToBottom () {
+  // Selectors
+  var messages = jQuery('#messages');
+  var newMessage = messages.children('li:last-child')
+  // Heights
+  var clientHeight = messages.prop('clientHeight');
+  var scrollTop = messages.prop('scrollTop');
+  var scrollHeight = messages.prop('scrollHeight');
+  var newMessageHeight = newMessage.innerHeight();
+  var lastMessageHeight = newMessage.prev().innerHeight();
+
+  if (clientHeight + scrollTop + newMessageHeight + lastMessageHeight >= scrollHeight) {
+    messages.scrollTop(scrollHeight);
+  }
+}
+
 socket.on('connect', function () {
   console.log('Connected to server');
 
@@ -14,12 +30,22 @@ socket.on('disconnect', function () {  // these are call back functions which ge
 });
 
 socket.on('newMessage', function (message) {
-	var getTime = moment(message.createdAt).format('h:mm a');
-  console.log('newMessage', message);
-  var li = jQuery('<li></li>');
-  li.text(`${message.from} ${getTime}: ${message.text}`);
+	var formattedTime = moment(message.createdAt).format('h:mm a');
+  var template = jQuery('#message-template').html();
+  var html = Mustache.render(template, {
+    text: message.text,
+    from: message.from,
+    createdAt: formattedTime
+  });
 
-  jQuery('#messages').append(li);
+  jQuery('#messages').append(html);
+  scrollToBottom();
+	// var getTime = moment(message.createdAt).format('h:mm a');
+ //  console.log('newMessage', message);
+ //  var li = jQuery('<li></li>');
+ //  li.text(`${message.from} ${getTime}: ${message.text}`);
+
+ //  jQuery('#messages').append(li);
 });
 
 jQuery('#message-form').on('submit', function (e) {
@@ -36,13 +62,23 @@ jQuery('#message-form').on('submit', function (e) {
 });
 
 socket.on('newLocationMessage', function (message) {
-  var li = jQuery('<li></li>');
-  var a = jQuery('<a target="_blank">My current location</a>');
+	var formattedTime = moment(message.createdAt).format('h:mm a');
+	var template = jQuery('#location-message-template').html();
+	var html = Mustache.render(template, {
+	    from: message.from,
+	    url: message.url,
+	    createdAt: formattedTime
+	 });
 
-  li.text(`${message.from}: `);
-  a.attr('href', message.url);
-  li.append(a);
-  jQuery('#messages').append(li);
+    jQuery('#messages').append(html);
+    scrollToBottom();
+  // var li = jQuery('<li></li>');
+  // var a = jQuery('<a target="_blank">My current location</a>');
+
+  // li.text(`${message.from}: `);
+  // a.attr('href', message.url);
+  // li.append(a);
+  // jQuery('#messages').append(li);
 });
 
 
